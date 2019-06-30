2cf08e285749871ba432cde8dec04019c107bc00
Paulo Henrique <paulohenriquejamsoft@jamsoft.com.br>
2019-06-30 08:40:49 -0300

    commit

---
+++ JTextLine/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace JTextLine
+{
+    partial class JTextLine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JTextLine));
+            this.txtTexto = new MetroFramework.Controls.MetroTextBox();
+            this.btnExecutar = new MetroFramework.Controls.MetroButton();
+            this.btnCopiarTexto = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // txtTexto
+            // 
+            // 
+            // 
+            // 
+            this.txtTexto.CustomButton.Image = null;
+            this.txtTexto.CustomButton.Location = new System.Drawing.Point(423, 1);
+            this.txtTexto.CustomButton.Name = "";
+            this.txtTexto.CustomButton.Size = new System.Drawing.Size(329, 329);
+            this.txtTexto.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtTexto.CustomButton.TabIndex = 1;
+            this.txtTexto.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtTexto.CustomButton.UseSelectable = true;
+            this.txtTexto.CustomButton.Visible = false;
+            this.txtTexto.Lines = new string[0];
+            this.txtTexto.Location = new System.Drawing.Point(24, 60);
+            this.txtTexto.MaxLength = 32767;
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.PasswordChar = '\0';
+            this.txtTexto.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtTexto.SelectedText = "";
+            this.txtTexto.SelectionLength = 0;
+            this.txtTexto.SelectionStart = 0;
+            this.txtTexto.ShortcutsEnabled = true;
+            this.txtTexto.Size = new System.Drawing.Size(753, 331);
+            this.txtTexto.Style = MetroFramework.MetroColorStyle.Black;
+            this.txtTexto.TabIndex = 0;
+            this.txtTexto.UseSelectable = true;
+            this.txtTexto.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtTexto.WaterMarkFont = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnExecutar.Location = new System.Drawing.Point(252, 397);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(145, 37);
+            this.btnExecutar.Style = MetroFramework.MetroColorStyle.Red;
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseSelectable = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnCopiarTexto
+            // 
+            this.btnCopiarTexto.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnCopiarTexto.Location = new System.Drawing.Point(403, 397);
+            this.btnCopiarTexto.Name = "btnCopiarTexto";
+            this.btnCopiarTexto.Size = new System.Drawing.Size(145, 37);
+            this.btnCopiarTexto.Style = MetroFramework.MetroColorStyle.Red;
+            this.btnCopiarTexto.TabIndex = 2;
+            this.btnCopiarTexto.Text = "Copiar";
+            this.btnCopiarTexto.UseSelectable = true;
+            this.btnCopiarTexto.Click += new System.EventHandler(this.btnCopiarTexto_Click);
+            // 
+            // JTextLine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCopiarTexto);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.txtTexto);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "JTextLine";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Text = "Jamsoft Text Line";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox txtTexto;
+        private MetroFramework.Controls.MetroButton btnExecutar;
+        private MetroFramework.Controls.MetroButton btnCopiarTexto;
+    }
+}
+
---
+++ JTextLine/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace JTextLine
+{
+    public partial class JTextLine : MetroFramework.Forms.MetroForm
+    {
+        public JTextLine()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            var texto = txtTexto.Text.Trim().Replace('\r', ' ').Replace('\n', ' ');
+            string novoTexto = "";
+            char caracterAnterior = '%';
+
+            // Adiciona um ESPACO após as virgulas para deixar tudo com um padrão só
+            foreach (char caracter in texto)
+            {
+                novoTexto += caracter == ',' ? caracter.ToString()+ " " : caracter.ToString();    
+            }
+
+            texto = novoTexto.Trim();
+            novoTexto = "";
+
+            // Remove ESPACOS extras
+            foreach (char caracter in texto)
+            {
+                if(caracter != caracterAnterior)
+                {
+                    caracterAnterior = caracter;
+                    novoTexto += caracter;
+                }
+            }
+
+            // Passando o Texto Formatado para o TextBox
+            txtTexto.Text = novoTexto.Trim();
+            txtTexto.Refresh();
+
+            MetroFramework.MetroMessageBox.Show(this, "Processo Finalizado com Sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.None, 100);
+        }
+
+        private void btnCopiarTexto_Click(object sender, EventArgs e)
+        {
+            var texto = txtTexto.Text.Trim();
+            if(texto.Length > 0)
+            {
+                Clipboard.SetText(texto);
+                MetroFramework.MetroMessageBox.Show(this, "Texto copiado!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.None,100);
+            }
+            else
+            {
+                MetroFramework.MetroMessageBox.Show(this, "Não existe Texto para ser copiado!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error,100);
+            }
+        }
+    }
+}
